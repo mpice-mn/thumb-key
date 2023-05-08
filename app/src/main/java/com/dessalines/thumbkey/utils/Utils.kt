9cd50645d34d2e392c6d49867ea9fd71902e6563
Simon Slater <sslater11@gmail.com>
2023-05-09 00:06:36 +0100

    Fixed cursor moving to the wrong position when inserting text longer than 1 character. Fixes #216

--- app/src/main/java/com/dessalines/thumbkey/utils/Utils.kt
+++ app/src/main/java/com/dessalines/thumbkey/utils/Utils.kt
@@ -187,7 +187,7 @@ fun performKeyAction(
             Log.d(TAG, "committing key text: $text")
             ime.currentInputConnection.commitText(
                 text,
-                text.length
+                1
             )
 
             if (autoCapitalize) {
